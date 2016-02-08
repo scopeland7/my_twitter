b9ad7e2c95ed4c3fd5f2eddbc7b179d112e7aee3
Sam Copeland <samcopeland7@gmail.com>
2016-02-08 13:43:54 -0500

    added functional feed page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root "epicenter#feed"
 
-  get 'show_user' => 'epicenter/show_user'
+  get 'show_user' => 'epicenter#show_user'
 
-  get 'now_following' => 'epicenter/now_following'
+  get 'now_following' => 'epicenter#now_following'
 
-  get 'unfollow' => 'epicenter/unfollow'
+  get 'unfollow' => 'epicenter#unfollow'
+
+  get 'tweets/new' => 'tweets#new'
 
   resources :tweets
   devise_for :users
