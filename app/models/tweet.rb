450cef3d977a06c4b45c2c2b60d25924f09764a5
Sam Copeland <samcopeland7@gmail.com>
2016-02-08 10:29:33 -0500

    did lots of crap

---
+++ app/models/tweet.rb
@@ -0,0 +1,6 @@
+class Tweet < ActiveRecord::Base
+	belongs_to :user
+
+	validates :message, presence: true, length: {maximum: 140, too_long: "A tweet is only 140 characters, you dummy!"}
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160208135701) do
+ActiveRecord::Schema.define(version: 20160208140716) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string   "message"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +33,10 @@ ActiveRecord::Schema.define(version: 20160208135701) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "username"
+    t.text     "bio"
+    t.string   "location"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
